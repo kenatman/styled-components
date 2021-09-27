2fb18b6bc899116b5da9f69a0f90ea5c1b5d5215
Yunsu <kenatman0723@gmail.com>
2021-09-27 14:26:34 +0900

    attrs(attributes) & mixin(css)

--- src/App.js
+++ src/App.js
@@ -1,60 +1,34 @@
-import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 body{padding:0;
   margin:0;}`;
 
-function App() {
-  return (
-    <Container>
-      <GlobalStyle />
-      <Button danger rotationTime={1}>
-        Hello
-      </Button>
-      <Button>Hello</Button>
-      <Anchor href="https://google.com">Go To Google</Anchor>
-    </Container>
-  );
-}
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
-  ${(props) => {
-    if (props.danger) {
-      return css`
-        animation: ${rotation} ${props.rotationTime}s linear infinite;
-      `;
-    }
-  }};
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
 `;
+
+const Input = styled.input.attrs({ required: true })`
+  border: none;
+  ${awesomeCard}
+`;
+
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-color: lightSalmon;
 `;
 
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-   from{
-     transform: rotate(0deg);
-   }
-   to{
-     transform: rotate(360deg);
-   }
- `;
+function App() {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Input placeholder="calm down... plz.." />
+    </Container>
+  );
+}
 
 export default App;
