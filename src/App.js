7da3427a9fc46d5ec9eac2c3856a35d08489af4f
Yunsu <kenatman0723@gmail.com>
2021-09-27 11:25:45 +0900

    new way : intro to styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <>
-      <Button danger></Button>
-      <Button></Button>
-    </>
+    <Container>
+      <Button danger>Hello</Button>
+      <Button>Hello</Button>
+    </Container>
   );
 }
 
-const Button = ({ danger }) => (
-  <button
-    className={danger ? "button button--danger" : "button button--success"}
-  >
-    Hello
-  </button>
-);
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: lightSalmon;
+`;
+
 export default App;
