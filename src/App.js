f8e9ca2611d1992f7c54b7bb0dfb86045423f449
Yunsu <kenatman0723@gmail.com>
2021-09-27 14:47:04 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import styled, { createGlobalStyle, css } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
 body{padding:0;
   margin:0;}`;
-
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-  border: none;
-  ${awesomeCard}
-`;
 
 const Container = styled.div`
   width: 100%;
@@ -22,12 +11,30 @@ const Container = styled.div`
   background-color: lightSalmon;
 `;
 
+const Card = styled.div`
+  background-color: tomato;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${(props) => props.theme.successColor};
+`;
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
+
 function App() {
   return (
-    <Container>
-      <GlobalStyle />
-      <Input placeholder="calm down... plz.." />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <GlobalStyle />
+        <Form></Form>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: "#54a0ff",
+  successColor: "#1dd1a1",
+  failureColor: "#ee5253",
+};
+
+export default theme;
